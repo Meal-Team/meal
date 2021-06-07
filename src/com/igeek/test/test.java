c803372346c229f92973ab6b6ced51c6b7e466f7
taoxiaofan <2489323694@qq.com>
2021-06-07 19:25:31 +0800

    第二次提交

--- src/com/igeek/test/test.java
+++ src/com/igeek/test/test.java
@@ -8,4 +8,8 @@ package com.igeek.test;
  * @Version
  */
 public class test {
+    public static void main(String[] args) {
+        System.out.println("你好!");
+        System.out.println("回退");
+    }
 }
